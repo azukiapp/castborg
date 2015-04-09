cead503aa2b6b250ca66c2e7ca2357b44384b30b
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-08 22:07:36 -0300

    index.js Added

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SourceCode : require('./lib/src/source-code'),
+  AstSearcher: require('./lib/src/ast-helpers/ast-searcher'),
+  AstModifier: require('./lib/src/ast-helpers/ast-modifier'),
+};
