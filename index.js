6feece366dfc2c8205928ec059ba0fe277cc6d50
Julio Makdisse Saito <saitodisse@gmail.com>
2015-04-08 22:29:17 -0300

    __esModule

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
+  __esModule: true,
+
   SourceCode : require('./lib/src/source-code'),
   AstSearcher: require('./lib/src/ast-helpers/ast-searcher'),
   AstModifier: require('./lib/src/ast-helpers/ast-modifier'),
